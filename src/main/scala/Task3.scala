4d49174dba4b1d9f9b323eaf58fc658d1e32da67
tp <katnak56@gmail.com>
2022-01-13 18:37:14 +0100

    Include Task 3 -> shingle words implementation

--- src/main/scala/Task3.scala
+++ src/main/scala/Task3.scala
@@ -1,5 +1,6 @@
 import scala.collection.immutable.ListMap
 
+
 object Task3 {
 
 
@@ -12,14 +13,36 @@ object Task3 {
       ("Crime and Punishment","src/main/scala/Books/Crime and Punishment.txt"),
       ("A Connecticut Yankee in King Arthur's Court.txt","src/main/scala/Books/A Connecticut Yankee in King Arthur's Court.txt"),
       ("Adventures of Huckleberry Finn.txt","src/main/scala/Books/Adventures of Huckleberry Finn.txt"),
-      ("The Adventures of Tom Sawyer.txt","src/main/scala/Books/The Adventures of Tom Sawyer.txt"))
+      ("The Adventures of Tom Sawyer.txt","src/main/scala/Books/The Adventures of Tom Sawyer.txt"),
+      ("Lorem Ipsum ","src/main/scala/Books/testFile.txt")
+    )
 
     val loader = new LoadBooks(books)
     val loadedBooks = loader.load()
-    printWords(5, loadedBooks)
 
-    val lastBook = loadedBooks.last
-    println(lastBook._3.take(100))
+    //books.foreach(book => generateShingles(book))
+
+    // this for some reason gives a List(Vector(HashSet))) when i expect List(HashSet))
+    val minShingle = 3
+    val maxShingle = 14
+    val shinglesInBooks = loadedBooks.map(book => (minShingle to maxShingle).map(generateShingles(book._3,_)))
+    val booksWithShingles = books.zip(shinglesInBooks)
+
+    booksWithShingles.combinations(2).foreach( twoBooks => {
+      val firstBook = twoBooks(0)
+      val secBook = twoBooks(1)
+      for (shingleIndex <- 0 to (maxShingle - minShingle)){
+        println("Jaccard Similarity of book " + firstBook._1._1 + " and " + secBook._1._1 + s" for ${shingleIndex + minShingle} shingles")
+        println(jaccSim(firstBook._2(shingleIndex), secBook._2(shingleIndex)))
+
+      }
+
+    })
+
+
+    //println(jaccSim(shinglesInBooks(0)(0), shinglesInBooks(1)(0)))
+
+
 
 
   }
@@ -29,6 +52,15 @@ object Task3 {
 
 }
 
+def jaccSim(set1: Set[List[String]], set2: Set[List[String]]) = {
+  set1.intersect(set2).size.toDouble / set1.union(set2).size
+}
+
+
+def generateShingles(book: List[String] ,ShingleSize: Int): Set[List[String]] = {
+  val shingles = book.sliding(ShingleSize).toSet
+  shingles
+}
 
 def printWords(n: Int, books: List[(String, Map[String, Int], _)]): Unit = {
   books.foreach(
